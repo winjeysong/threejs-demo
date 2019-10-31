bf1994b06d702b2a3dfbfa3deee95a7827dc832d
songwj <winjey-song@163.com>
2019-10-31 16:55:42 +0800

    完成带有辅助视图的简易太阳系

--- utils/index.js
+++ utils/index.js
@@ -9,3 +9,30 @@ function resizeRendererToDisplaySize(renderer) {
     }
     return needResize;
 }
+
+class AxisGridHelper {
+    constructor(node, visible = false, units) {
+        const axes = new THREE.AxesHelper(); // x(red) y(green) z(blue)
+        axes.material.depthTest = false; // 取消深度，即使轴在内部也能看到
+        axes.renderOrder = 2; // 绘制顺序，需要在辅助网格绘制完成后再绘制辅助轴（默认为0）
+        node.add(axes);
+
+        const grid = new THREE.GridHelper(units, units);
+        grid.material.depthTest = false;
+        grid.renderOrder = 1; // 绘制顺序，需要在球体绘制完成后再绘制辅助网格（默认为0）
+        node.add(grid);
+
+        this.grid = grid;
+        this.axes = axes;
+        this.visible = visible;
+    }
+
+    get visible() {
+        return this._visible;
+    }
+    set visible(v) {
+        this._visible = v;
+        this.grid.visible = v;
+        this.axes.visible = v;
+    }
+}
