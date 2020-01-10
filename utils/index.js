1dbee0e224d4f2249e34f271460a501e5901169a
songwj <winjey-song@163.com>
2020-01-10 14:38:09 +0800

    add new demo

--- utils/index.js
+++ utils/index.js
@@ -139,3 +139,25 @@ export function frameArea(sizeToFitScreen, boxSize, boxCenter, camera) {
 
     camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
 }
+
+function dumpVec3(v3, precision = 3) {
+    return `${v3.x.toFixed(precision)}, ${v3.y.toFixed(precision)}, ${v3.z.toFixed(precision)}`;
+}
+
+export function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const dataPrefix = obj.children.length
+        ? (isLast ? '  │ ' : '│ │ ')
+        : (isLast ? '    ' : '│   ');
+    lines.push(`${prefix}${dataPrefix}  pos: ${dumpVec3(obj.position)}`);
+    lines.push(`${prefix}${dataPrefix}  rot: ${dumpVec3(obj.rotation)}`);
+    lines.push(`${prefix}${dataPrefix}  scl: ${dumpVec3(obj.scale)}`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+        const isLast = ndx === lastNdx;
+        dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+}
